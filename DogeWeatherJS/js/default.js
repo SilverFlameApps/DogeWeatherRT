12581642bc4a29699aac8ea984d60f11001f71b4
SilverFlameApps <silverflamenet@gmail.com>
2014-02-12 09:09:36 +0100

    First edit, needs minors things before publish.

---
+++ DogeWeatherJS/js/default.js
@@ -0,0 +1,113 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+
+//                  Port By:
+
+// Developer:       Christian Kristensen
+// Company:         SilverFlame Apps
+// Data:            February 2014
+// github:          
+
+//                  Much Happy, Many Thanks! :)
+// Orignial:        http://www.dogeweather.com
+// github:          https://github.com/katiaeirin/dogeweather  
+
+
+(function () {
+    "use strict";
+    var page = WinJS.UI.Pages.define("/default.html", {
+        ready: function (element, options) {
+            //document.getElementById("launchUriWithWarningButton").addEventListener("click", launchUriWithWarning, false);
+        }
+    });
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    var uriToLaunch = "ms-windows-store:REVIEW?PFN=[f9962804-4ab7-4d9d-9a08-9b303ba2b251]"; //Edit for your Application!
+
+    var applicationData = Windows.Storage.ApplicationData.current;
+    var localSettings = applicationData.localSettings;
+    var localFolder = applicationData.localFolder;
+
+    app.onready = function () {
+        readRateMeData();
+
+        //Test Rating
+        //ShowMessage();
+    }
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+               
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    function ShowMessage() {
+        var msgpopup = new Windows.UI.Popups.MessageDialog("much happy, little rate?", "wow application?");
+        msgpopup.commands.append(new Windows.UI.Popups.UICommand("so wow", function () { launchUri(uriToLaunch); }));
+        msgpopup.commands.append(new Windows.UI.Popups.UICommand("much sad", function () { }));
+        msgpopup.showAsync();
+    }
+
+    function writeRateMeData(text) {
+        localSettings.values["RateMe"] = text;
+    }
+
+    function readRateMeData() {
+        var value = localSettings.values["RateMe"];
+
+        if (!value) {
+            console.log("Nothing Found");
+            // Rate Me Data not found
+            writeRateMeData("0"); //First time, well write a zero.
+            ShowMessage();
+        }
+        else {
+            console.log(value);
+            // Rate Me is contained in data
+            if (value == "0") { //Second Time (Let's ask for review)
+                writeRateMeData("1");
+            }
+            else if (value == "1") { //Third Time (Already asked, don't ask again :C)
+            }
+        }
+    }
+
+    function launchUri(link) {
+        // Create the URI to launch from a string.
+        var uri = new Windows.Foundation.Uri(link);
+
+        // Set the show warning option.
+        //var options = new Windows.System.LauncherOptions();
+        //options.treatAsUntrusted = true;
+
+        // Launch the URI.
+        Windows.System.Launcher.launchUriAsync(uri).done(
+            function (success) {
+                if (success) {
+                    WinJS.log && WinJS.log("URI " + uri.absoluteUri + " launched.", "sample", "status");
+                } else {
+                    WinJS.log && WinJS.log("URI launch failed.", "sample", "error");
+                }
+            });
+    }
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.start();
+})();
---
+++ DogeWeatherJS/js/main.js
@@ -0,0 +1,84 @@
+//getWeather("./weather.php");
+getLocation();
+
+	   function getWeather(link) {
+	       jQuery.support.cors = true;
+
+	       WinJS.xhr({ url: link }).then(function (data) {
+	   		//$.getJSON(link, function(data){
+
+			//console.log(data);
+
+			var obj = jQuery.parseJSON(data.responseText);
+
+			//set weather id & icon 
+			var id = obj.weather[0].id;
+			var icon = obj.weather[0].icon;
+
+			$('#weather-id').text(id);
+			$('#weather-icon').text(icon);
+
+			//TESTING 
+			//icon = "01n";
+			//change such doge and sky based on much icon
+			var doge_img = "url(./img/doge/" + icon + ".png)";
+			$('.doge-image').css('background-image', doge_img);
+
+			var sky_img = "url(./img/sky-img/" + icon + ".png)";
+			$('.bg').css('background-image', sky_img);
+
+			//console.log(icon);
+
+			//get weather description
+			var tempCelcius = obj.main.temp - 273.15;
+			var tempFahrenheit = tempCelcius * 9 / 5 + 32;
+			var description = obj.weather[0].description;
+
+			$('#weather-desc').text("wow " + description);
+			$('#location').text(obj.name);
+
+			$('#degreesCelsius .number').text(Math.round(tempCelcius));
+			$('#degreesCelsius .cel').text("°C ");
+			$('#degreesFahrenheit').text(Math.round(tempFahrenheit) + "°F");
+
+			$(".suchlikes").show();
+			$(".ourinfo").show();
+
+			//initialise such doge
+			$($.doge);
+		});
+	   }
+
+	   $("#browser_geo").on('click', function () {
+	       var text = $("#browser_geo").text();
+	       if (text != "wow, located!") {
+	       getLocation();
+	        }
+        });
+
+	   	function getLocation()
+			  {
+			  if (navigator.geolocation)
+			    {
+			    navigator.geolocation.getCurrentPosition(showPosition);
+			    }
+			  else
+			  	$("#browser_geo").text("Geolocation is not supported by this browser.");
+			  }
+			function showPosition(position)
+			  {
+			  //$("#browser_geo").text("Latitude: " + position.coords.latitude + 
+			  //"Longitude: " + position.coords.longitude);
+
+			  	var url = 'http://api.openweathermap.org/data/2.5/weather';
+			  	url += '?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '?format=json';
+
+                getWeather(url);
+                $("#browser_geo").text("wow, located!").css("cursor", "auto").css("color", "#B66DFF"); 
+			  }
+			
+
+
+
+
+
